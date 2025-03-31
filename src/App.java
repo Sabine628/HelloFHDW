ab036b1fd3430dd056c0729905b4a6265678dcca
fhdw-pr <philipp.renerig@fhdw.de>
2025-03-31 12:52:01 +0200

    Add HelloFHDW

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        HelloFHDW.variables();
+        HelloFHDW.strings();
+        HelloFHDW.arithmeticOperators();
+        HelloFHDW.cast();
+        HelloFHDW.logicalOperators();
+        HelloFHDW.conditions();
+        HelloFHDW.branches();
+        HelloFHDW.selection();
+        HelloFHDW.whileLoop();
+    }
+}
